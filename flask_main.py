5cd855314601e0c31a373ff028970656fe304bf1
konradsiebor <konradsiebor@gmail.com>
2021-03-27 19:07:10 -0400

    Integrated Boostrap

--- flask_main.py
+++ flask_main.py
@@ -1,6 +1,8 @@
 from flask import *
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def home_page():
@@ -8,7 +10,24 @@ def home_page():
 
 @app.route('/form', methods=['GET', 'POST'])
 def submission_form():
-    return render_template('form.html')
+    if request.method == 'GET':
+        return render_template('form.html')
+    elif request.method == 'POST':
+        try:
+            ticker_symbol = request.form.get("ticker_symbol", None)
+            value = request.form.get("value", None)
+            current_risk = request.form.get("current_risk", None)
+            future_risk = request.form.get("future_risk", None)
+            return redirect(url_for('recommendation_page', ticker_symbol=ticker_symbol, value=value, current_risk=current_risk, future_risk=future_risk))
+        except:
+            print("Failed submission")
+            # write_error_log("Failed submission")
+            return redirect(url_for('home_page'))
+
+@app.route('/recommendation/<ticker_symbol>/<value>/<current_risk>/<future_risk>', methods=['GET', 'POST'])
+def recommendation_page(ticker_symbol, value, current_risk, future_risk):
+    if request.method == 'GET':
+        return render_template('recommendation.html', ticker_symbol=ticker_symbol, value=value, current_risk=current_risk, future_risk=future_risk)
 
 if __name__ == "__main__":
     from waitress import serve
