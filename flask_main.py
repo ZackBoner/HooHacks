2e53ac58a5e6defac052fbedef9c79bf833af86e
konradsiebor <konradsiebor@gmail.com>
2021-03-27 14:50:23 -0400

    Added general website structure

--- flask_main.py
+++ flask_main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home_page():
+    return render_template('home.html')
 
-if __name__ == '__main__':
+@app.route('/form', methods=['GET', 'POST'])
+def submission_form():
+    return render_template('form.html')
+
+if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
-    #app.run(debug=True)
