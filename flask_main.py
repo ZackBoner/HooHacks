00c73eefddd4306cd6adcf1a7196d1c497eeb80b
konradsiebor <konradsiebor@gmail.com>
2021-03-27 20:46:45 -0400

    Formated the home page, form submissions, and reccomendation page

--- flask_main.py
+++ flask_main.py
@@ -8,21 +8,24 @@ Bootstrap(app)
 def home_page():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/form', methods=['GET', 'POST'])
 def submission_form():
     if request.method == 'GET':
         return render_template('form.html')
     elif request.method == 'POST':
-        try:
+        # try:
             ticker_symbol = request.form.get("ticker_symbol", None)
             value = request.form.get("value", None)
             current_risk = request.form.get("current_risk", None)
             future_risk = request.form.get("future_risk", None)
             return redirect(url_for('recommendation_page', ticker_symbol=ticker_symbol, value=value, current_risk=current_risk, future_risk=future_risk))
-        except:
-            print("Failed submission")
-            # write_error_log("Failed submission")
-            return redirect(url_for('home_page'))
+        # except:
+        #     print("Failed submission")
+        #     return redirect(url_for('home_page'))
 
 @app.route('/recommendation/<ticker_symbol>/<value>/<current_risk>/<future_risk>', methods=['GET', 'POST'])
 def recommendation_page(ticker_symbol, value, current_risk, future_risk):
