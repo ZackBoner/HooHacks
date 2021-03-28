e3590dbc7fc32dc58e0c86a42e4d5edeb664be99
konradsiebor <konradsiebor@gmail.com>
2021-03-27 21:02:11 -0400

    Added invalid_form redirect

--- flask_main.py
+++ flask_main.py
@@ -17,15 +17,19 @@ def submission_form():
     if request.method == 'GET':
         return render_template('form.html')
     elif request.method == 'POST':
-        # try:
+        try:
             ticker_symbol = request.form.get("ticker_symbol", None)
             value = request.form.get("value", None)
             current_risk = request.form.get("current_risk", None)
             future_risk = request.form.get("future_risk", None)
             return redirect(url_for('recommendation_page', ticker_symbol=ticker_symbol, value=value, current_risk=current_risk, future_risk=future_risk))
-        # except:
-        #     print("Failed submission")
-        #     return redirect(url_for('home_page'))
+        except:
+            print("Failed submission")
+            return redirect(url_for('home_page'))
+
+@app.route('/invalid_form')
+def invalid_form():
+    return render_template('invalid_form.html')
 
 @app.route('/recommendation/<ticker_symbol>/<value>/<current_risk>/<future_risk>', methods=['GET', 'POST'])
 def recommendation_page(ticker_symbol, value, current_risk, future_risk):
